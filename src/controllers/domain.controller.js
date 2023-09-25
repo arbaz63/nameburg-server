56806b102c60606b4ed7e364be88313798bb299b
arbaz63 <arbazsheraz05555@gmail.com>
2023-09-25 15:19:48 +0500

    fixes

--- src/controllers/domain.controller.js
+++ src/controllers/domain.controller.js
@@ -11,7 +11,7 @@ const getDomains = async (req, res) => {
     const totalPages = Math.ceil(totalDomains / req.limit);
 
     const domains = await req.query
-    .select('image date description name views sold currentPrice keywords')  
+    .select('image maxPrice date description name views sold currentPrice keywords')  
     .skip((req.page - 1) * req.limit)
       .limit(req.limit)
 
@@ -29,7 +29,7 @@ const getDomains = async (req, res) => {
 
 const getDomain = async (req, res) => {
   try {
-    const domain = await Domain.findById(req.params.id).populate("category").select('bigImage date description name views sold currentPrice keywords')  ;
+    const domain = await Domain.findById(req.params.id).populate("category").select('bigImage maxPrice date description name views sold currentPrice keywords')  ;
     if (!domain) {
       return res.status(404).json({ message: "Domain not found" });
     }
